95c8512b99336130355288781bc31cd942629bb8
taroken65 <yonemurano@gmail.com>
2021-08-01 15:09:12 +0000

    0801 commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
-  before_action :check_user, {only: :edit}
+  
   def index
    @users = User.all
    @user = current_user
+   
    @book = Book.new
+   @followings = @user.followings
+   @followers = @user.followers
+   
   end
   
   def show
    @user = User.find(params[:id])
    @books = @user.books
    @book = Book.new
+   @followings = @user.followings
+   @followers = @user.followers
   end
   
   def edit
@@ -29,17 +35,13 @@ class UsersController < ApplicationController
     end
   end
   
+  
  private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
-  def check_user
-    @user = User.find(params[:id])
-    if @user.id != current_user.id
-       redirect_to user_path(current_user.id)
-    end
-  end 
+  
   
 end
